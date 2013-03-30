38d3a8fdf9a3d42283b8f9208c0e64d89e7cde87
Krzysztof Cywiński <krzysztof_cywinski@o2.pl>
2013-03-30 20:22:37 +0100

    Form based login.

---
+++ src/Wsh/WebsiteBundle/Controller/SecurityController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Wsh\WebsiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
+use Wsh\WebsiteBundle\Entity;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     * @Template()
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function loginAction()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContext::AUTHENTICATION_ERROR
+            );
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return array(
+                // last username entered by the user
+                'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+                'error'         => $error,
+        );
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutAction()
+    {
+        $this->get('session')->getFlashBag()->add('success', 'You have been logged out');
+        return $this->redirect($this->generateUrl('homepage'));
+    }
+}
